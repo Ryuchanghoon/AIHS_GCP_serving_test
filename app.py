d79f231b36bc73357c97e76619ea4535461c6539
fbckdgns3@naver.com <fbckdgns3@naver.com>
2024-02-08 17:21:22 +0900

    [fix] css 참조 오류 해결.

--- app.py
+++ app.py
@@ -7,30 +7,22 @@ from io import BytesIO
 app = Flask(__name__)
 
 def process_image(image):
-    #  YCrCb 컬러 스페이스 변환
     ycrcb_image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
-    # Y 채널 CLAHE 적용
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)) #이미지 퀄리티 향상 위한 CLAHE
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     Y_channel, Cr, Cb = cv2.split(ycrcb_image)
     Y_channel = clahe.apply(Y_channel)
-    # 변경된 Y 채널 다시 YCrCb 병합
     merged_ycrcb = cv2.merge([Y_channel, Cr, Cb])
-    # YCrCb에서 BGR 컬러 스페이스 변환
     final_image = cv2.cvtColor(merged_ycrcb, cv2.COLOR_YCrCb2BGR)
-    # streamlit은 RGB형태로 이미지 불어들여옴. 그래서 억지로. 밑에.
     rgb_image = cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB)
     return rgb_image
 
 def convert_image_to_grayscale(image):
-    # 흑백 이미지 변환
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return grayscale_image
-
 
 def flip_image(image):
     flipped_image = cv2.flip(image, 0)
     rgb_flipped_image = cv2.cvtColor(flipped_image, cv2.COLOR_BGR2RGB)
-
     return rgb_flipped_image
 
 
@@ -42,10 +34,12 @@ def plot_histograms(original_image, processed_image):
     channels = ('Y', 'Cr', 'Cb')
 
     fig, axs = plt.subplots(2, 3, figsize=(16, 6))
-
+    
+    pass  
 
 
 @app.route('/', methods=['GET', 'POST'])
+
 def index():
     if request.method == 'POST':
         uploaded_file = request.files['file']
@@ -59,24 +53,20 @@ def index():
 
             if option == 'Histogram Equalization':
                 processed_image = process_image(image)
-                plt_image = plot_histograms(image, processed_image)
-                buf = BytesIO()
-                plt.savefig(buf, format='png')
-                buf.seek(0)
-                return send_file(buf, mimetype='image/png')
+                _, img_encoded = cv2.imencode('.png', processed_image)
+                return send_file(BytesIO(img_encoded), download_name='processed.png', as_attachment=True)
 
             elif option == '흑백변환':
                 grayscale_image = convert_image_to_grayscale(image)
                 _, img_encoded = cv2.imencode('.png', grayscale_image)
-                return send_file(BytesIO(img_encoded), attachment_filename='grayscale.png', as_attachment=True)
+                return send_file(BytesIO(img_encoded), download_name='grayscale.png', as_attachment=True)
 
             elif option == '90도 회전':
                 flipped_image = flip_image(image)
                 _, img_encoded = cv2.imencode('.png', flipped_image)
-                return send_file(BytesIO(img_encoded), attachment_filename='flipped.png', as_attachment=True)
+                return send_file(BytesIO(img_encoded), download_name='flipped.png', as_attachment=True)
 
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
